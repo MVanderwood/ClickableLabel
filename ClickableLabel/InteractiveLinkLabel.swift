3f748f98f2e6c159cc3e2c5546861eecca85a080
MVanderwood <m.b.vanderwood@gmail.com>
2020-07-30 12:55:40 -0600

    Add functionality to auto-detect hyperlinks in text, and shifted code into InteractiveLinkLabel for easy use.

--- ClickableLabel/InteractiveLinkLabel.swift
+++ ClickableLabel/InteractiveLinkLabel.swift
@@ -9,45 +9,81 @@
 import UIKit
 
 class InteractiveLinkLabel: UILabel {
-    
+
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
         self.configure()
+        self.detectHyperlinks()
     }
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.configure()
+        self.detectHyperlinks()
     }
-    
+
     func configure() {
         isUserInteractionEnabled = true
     }
-    
+
+    private func detectHyperlinks() {
+        guard
+            let detector = try? NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue),
+            let text = text
+        else { return }
+
+        let matches = detector.matches(in: text, options: [], range: NSRange(location: 0, length: text.utf16.count))
+
+        var urls = [Substring]()
+        for match in matches {
+            guard let range = Range(match.range, in: text) else { continue }
+            urls.append(text[range])
+        }
+
+        let output = NSMutableAttributedString()
+        for url in urls {
+            let url = String(url)
+            let textComponents = text.components(separatedBy: url)
+            output.append(NSAttributedString(string: textComponents[0], attributes: nil))
+
+            guard let correctedUrl = URL(string: String(url.hasPrefix("http") ? url : "http://\(url)")) else { continue }
+            output.append(NSAttributedString(string: String(url), attributes: [NSAttributedString.Key.link: correctedUrl]))
+
+            if textComponents.count > 2 {
+                let tailComponents = textComponents[2..<(textComponents.count - 1)]
+                let tailArray = Array(tailComponents)
+                let tail = tailArray.joined(separator: url)
+                output.append(NSAttributedString(string: tail, attributes: nil))
+            }
+        }
+
+        attributedText = output
+    }
+
     override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
-        
+
         let superBool = super.point(inside: point, with: event)
-        
+
         // Configure NSTextContainer
         let textContainer = NSTextContainer(size: bounds.size)
         textContainer.lineFragmentPadding = 0.0
         textContainer.lineBreakMode = lineBreakMode
         textContainer.maximumNumberOfLines = numberOfLines
-        
+
         // Configure NSLayoutManager and add the text container
         let layoutManager = NSLayoutManager()
         layoutManager.addTextContainer(textContainer)
-        
+
         guard let attributedText = attributedText else {return false}
-        
+
         // Configure NSTextStorage and apply the layout manager
         let textStorage = NSTextStorage(attributedString: attributedText)
         textStorage.addAttribute(NSAttributedString.Key.font, value: font!, range: NSMakeRange(0, attributedText.length))
         textStorage.addLayoutManager(layoutManager)
-        
+
         // get the tapped character location
         let locationOfTouchInLabel = point
-        
+
         // account for text alignment and insets
         let textBoundingBox = layoutManager.usedRect(for: textContainer)
         var alignmentOffset: CGFloat!
@@ -61,32 +97,31 @@ class InteractiveLinkLabel: UILabel {
         @unknown default:
             fatalError()
         }
-        
+
         let xOffset = ((bounds.size.width - textBoundingBox.size.width) * alignmentOffset) - textBoundingBox.origin.x
         let yOffset = ((bounds.size.height - textBoundingBox.size.height) * alignmentOffset) - textBoundingBox.origin.y
         let locationOfTouchInTextContainer = CGPoint(x: locationOfTouchInLabel.x - xOffset, y: locationOfTouchInLabel.y - yOffset)
-        
+
         // work out which character was tapped
         let characterIndex = layoutManager.characterIndex(for: locationOfTouchInTextContainer, in: textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
-        
+
         // work out how many characters are in the string up to and including the line tapped, to ensure we are not off the end of the character string
         let lineTapped = Int(ceil(locationOfTouchInLabel.y / font.lineHeight)) - 1
         let rightMostPointInLineTapped = CGPoint(x: bounds.size.width, y: font.lineHeight * CGFloat(lineTapped))
         let charsInLineTapped = layoutManager.characterIndex(for: rightMostPointInLineTapped, in: textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
-        
+
         guard characterIndex < charsInLineTapped else {return false}
-        
+
         let attributeName = NSAttributedString.Key.link
-        
+
         let attributeValue = self.attributedText?.attribute(attributeName, at: characterIndex, effectiveRange: nil)
-        
+
         if let value = attributeValue {
             if let url = value as? URL {
                 UIApplication.shared.open(url)
             }
         }
-        
+
         return superBool
-        
     }
 }
--- ClickableLabel/ViewController.swift
+++ ClickableLabel/ViewController.swift
@@ -9,58 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var gestureLabel: UILabel!
-    @IBOutlet weak var gestureTextView: UITextView!
-    @IBOutlet weak var attributedTextView: UITextView!
-    @IBOutlet weak var attributedLabel: UILabel!
-    @IBOutlet weak var interactiveLinkLabel: InteractiveLinkLabel!
-    @IBOutlet weak var interactiveLinkTextView: UITextView!
+    @IBOutlet weak var testLabel1: InteractiveLinkLabel!
+    @IBOutlet weak var testLabel2: InteractiveLinkLabel!
+    @IBOutlet weak var testLabel3: InteractiveLinkLabel!
+    @IBOutlet weak var testLabel4: InteractiveLinkLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Tap gesture recognizers
-        let tapLabel = UITapGestureRecognizer(target: self, action: #selector(ViewController.tapFunction))
-        let tapTV = UITapGestureRecognizer(target: self, action: #selector(ViewController.tapFunction))
-
-        // Gesture recognizer Label
-        gestureLabel.isUserInteractionEnabled = true
-        gestureLabel.addGestureRecognizer(tapLabel)
-        gestureLabel.text = "Gesture recognizer label"
-
-        // Gesture recognizer TextView
-        gestureTextView.isUserInteractionEnabled = true
-        gestureTextView.isEditable = false
-        gestureTextView.addGestureRecognizer(tapTV)
-        gestureTextView.text = "Gesture recognizer text view"
-        
-        let plainAttributedString = NSMutableAttributedString(string: "This is a link: ", attributes: nil)
-        let string = "A link to Google"
-        let attributedLinkString = NSMutableAttributedString(string: string, attributes:[NSAttributedString.Key.link: URL(string: "http://www.google.com")!])
-        let fullAttributedString = NSMutableAttributedString()
-        fullAttributedString.append(plainAttributedString)
-        fullAttributedString.append(attributedLinkString)
-        attributedLabel.isUserInteractionEnabled = true
-        attributedLabel.attributedText = fullAttributedString
-        
-        attributedTextView.isUserInteractionEnabled = true
-        attributedTextView.isEditable = false
-        attributedTextView.attributedText = fullAttributedString
-        
-        interactiveLinkLabel.attributedText = fullAttributedString
-        
-        interactiveLinkTextView.isUserInteractionEnabled = true
-        interactiveLinkTextView.isEditable = false
-        interactiveLinkTextView.attributedText = fullAttributedString
     }
-    
-    @IBAction func tapFunction(sender: UITapGestureRecognizer) {
-        let url = URL(string: "http://www.google.com")!
-        UIApplication.shared.open(url)
-
-    }
-
-
 }
 
